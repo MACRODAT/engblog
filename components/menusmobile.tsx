8d3f143fb90ae4950fb9b33d741b7dd24e348625
MACRODAT <macrodatservices@gmail.com>
2025-01-08 19:32:54 +0100

    Changes to the file

--- components/menusmobile.tsx
+++ components/menusmobile.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback} from 'react';
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
-function MenusMobile() {
+function MenusMobile({mainScreen}: any) {
   const handle = useFullScreenHandle();
 
   return (
@@ -10,8 +10,11 @@ function MenusMobile() {
         Enter fullscreen
       </button>
 
-      <FullScreen handle={handle}>
+      <FullScreen handle={handle} className='my-component'>
         Any fullscreen content here
+		<button onClick={handle.exit}>
+            Exit
+		</button>
       </FullScreen>
     </div>
   );
